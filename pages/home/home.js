67d546d0b0bb76bf4de87d4ebfedc4fae6cf88a0
wxyzcctv <2846629226@qq.com>
2020-06-09 11:11:29 +0800

    增加todolist的增删

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,43 +1,36 @@
 Page({
 
   data: {
-    visible:true
+    visible:false,
+    lists:[
+      { id:1, text:"你好，我今天的计划1", finished:true },
+      { id:2, text:"你好，我今天的计划2", finished:true },
+      { id:3, text:"你好，我今天的计划3", finished:false },
+      { id:4, text:"你好，我今天的计划4", finished:false },
+      { id:5, text:"你好，我今天的计划5", finished:true },
+      { id:6, text:"你好，我今天的计划6", finished:false },
+    ]
   },
   confirm(event){
-    console.log(event.detail)
+    let content = event.detail
+    if(content){
+      let todo = [{ id: this.data.lists.length + 1, text: content, finished: false}]
+      let newLists = todo.concat(this.data.lists)
+      this.setData({ lists: newLists })
+      this.hiddenConfirm()
+    }
+    // 按下确认键，增加内容
   },
-  cancel(){
+  destroyItem(e){
+    let index = e.currentTarget.dataset.index
+    this.data.lists[index].finished = true
+    this.setData({ lists: this.data.lists })
+    // 按下选项，选项转换为true
+  },
+  hiddenConfirm(){
     this.setData({visible:false})
   },
-  onLoad: function (options) {
-
-  },
-
-  onReady: function () {
-
-  },
-
-  onShow: function () {
-
-  },
-
-  onHide: function () {
-
-  },
-
-  onUnload: function () {
-
-  },
-
-  onPullDownRefresh: function () {
-
-  },
-
-  onReachBottom: function () {
-
-  },
-
-  onShareAppMessage: function () {
-
+  showConfirm(){
+    this.setData({ visible: true})
   }
 })
