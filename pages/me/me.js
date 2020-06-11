980b254f704881e58ea58a462e95f22085bee1e0
wxyzcctv <2846629226@qq.com>
2020-06-11 11:40:49 +0800

    完成我的页面基本布局

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,65 +1,53 @@
 // pages/me/me.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tab:"tomato",
+    lists:{
+      "周三":[
+        {time:"14:00",text:"英雄都是战胜自己畏惧心理的人，这并不是一件容易的事儿",id:1},
+        {time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:2},
+        {time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:3},
+        {time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:4}
+      ],
+      "周四":[{time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:1}],
+      "周五":[{time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:1}],
+      "周六":[{time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:1}],
+      "周天":[{time:"14:00",text:"英雄都是战胜自己畏惧心理的人",id:1}]
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  changeTab(event){
+    let name = event.currentTarget.dataset.name
+    this.setData({ tab: name})
+  },
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
